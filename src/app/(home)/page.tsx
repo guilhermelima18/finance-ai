7af61c1fd2e95bccad7d76dae544e1b717016429
guilhermelima18 <guilhermelima18@hotmail.com>
2024-11-06 20:36:23 -0300

    feat: config models of the database

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Finance AI</h1>
+    </div>
+  );
+}
---
+++ src/app/subscription/page.tsx
@@ -0,0 +1,7 @@
+export default function Subscription() {
+  return (
+    <div>
+      <h1>Subscription</h1>
+    </div>
+  );
+}
